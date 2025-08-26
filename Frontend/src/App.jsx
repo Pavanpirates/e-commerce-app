6a48290c0d49dd3aa519cd196901c530a7371c3b
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-08-26 23:10:20 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+ <div class="bg-gray-100 p-6">
+
+  <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+  <p class="mt-2 text-gray-700">This is a basic Tailwind CSS working example.</p>
+</div>
+    
+  )
+}
+
+export default App
