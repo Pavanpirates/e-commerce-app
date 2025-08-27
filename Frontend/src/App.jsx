3bf300b7ecfe38fa6cb5065c601078959ca23566
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-08-27 11:46:43 +0530

    Created Pages and Routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
- <div class="bg-gray-100 p-6">
-
-  <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
-  <p class="mt-2 text-gray-700">This is a basic Tailwind CSS working example.</p>
-</div>
     
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path= '/about' element={<About/>}/>
+        <Route path= '/contact' element={<Contact/>}/>
+        <Route path= '/product/:productId' element={ <Product/>}/>
+        <Route path= '/cart' element={ <Cart/>}/>
+        <Route path= '/login' element={ <Login/>}/>
+        <Route path='place-order' element={ <PlaceOrder/>}/>
+        <Route path= 'Orders' element={ <Orders/>}/>
+      </Routes>
+    </div>
   )
 }
 
