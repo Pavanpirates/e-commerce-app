e767e27f853ba516be510a6f449d7c03f7eb5fbe
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-09-23 00:29:30 +0530

    Order page & Login page completed

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign Up');
+  const onSubmitHandler = async(event)=>{
+       event.preventDefault(); 
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+      {currentState === 'Login' ? '' :<input 
+        type="text" 
+        className="w-full px-3 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-colors duration-200" 
+        placeholder="Name" 
+        required 
+      /> }  
+
+      <input 
+        type="email" 
+        className="w-full px-3 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-colors duration-200" 
+        placeholder="Email" 
+        required 
+      />
+
+      <input 
+        type="password" 
+        className="w-full px-3 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-colors duration-200" 
+        placeholder="Password" 
+        required 
+      />
+        <div className='w-full flex  justify-between text-sm mt-[-8px]'>
+          <p className='cursor-pointer'>Forgot your password?</p>
+          {
+            currentState === 'Login' 
+            ? <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create account</p>
+            : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+          }
+        </div>
+         <button className="bg-black text-white font-light px-8 py-2 mt-4 rounded-md shadow-md hover:bg-gray-800 transition-colors duration-200">
+            {currentState === 'Login' ? 'Sign In' : 'Sign Up'}
+          </button>
+
+
+    </form>
   )
 }
 
