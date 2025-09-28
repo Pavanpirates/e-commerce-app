43a8844e78936bf919bddceedcce386ddae1e62d
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-09-28 09:50:45 +0530

    product controllers completed

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,4 +1,6 @@
-
+import { v2 as cloudinary } from 'cloudinary';
+import { image } from 'qr-image';
+import productModel from "../models/productModel.js"
 
 
 
@@ -12,11 +14,33 @@
                 const image3 = req.files.image3 && req.files.image3[0]
                     const image4 = req.files.image4 && req.files.image4[0]
 
-                    console.log(name, description, price, category, subCategory,sizes, bestseller)
-                    console.log(image1, image2, image3, image4); 
+                  const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+
+                   
+                     let imagesUrl = await Promise.all(
+                          images.map(async (item) => {
+                       let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
+                      return result.secure_url;
+                         })
+                           );
+
+                     const productData ={
+                        name,
+                        description,
+                        category,
+                        price: Number(price),
+                        subCategory,
+                        bestseller: bestseller === "true" ? true : false,
+                        sizes: JSON.parse(sizes),
+                        images: imagesUrl,
+                        date: Date.now()
+                     }
+                      console.log(productData);
                     
-                    
-                    res.json({})
+                    const product = new productModel(productData);
+                     await product.save()
+
+                    res.json({success:true, message:"Product Added"})
        } catch (error) {
             console.log(error)
            res.json({success:false, message:error.message})
@@ -27,13 +51,38 @@
 
  //Function for list products
  const listProducts = async (req, res) => {
+
+     try {
+
+       const products = await productModel.find({});
+       res.json({success: true, products})
+      
+     } catch (error) {
+        console.log(error)
+           res.json({success:false, message:error.message})
+      
+     }
+
  }
+
+
 
 
 
  //funstion for remove a product
 
     const removeProduct = async (req, res) => {
+
+           try {  
+                  await productModel.findByIdAndDelete(req.body.id)
+                  res.json({success:true,message: "Product Removed"})
+            
+           } catch (error) {
+             console.log(error)
+           res.json({success:false, message:error.message})
+            
+           }  
+
     }
 
 
@@ -43,6 +92,19 @@
     //function for single product details
 
     const singleProduct = async (req, res) => {
+              
+                try {
+                        const {productId}= req.body
+                        const product = await productModel.findById(productId)
+                        res.json({success:true, product})
+                  
+                } catch (error) {
+
+                    console.log(error)
+           res.json({success:false, message:error.message})
+                  
+                }
+
     }
 
 
