85d3b9fae58da50a2f524983807232cb570bd4d8
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-10-19 01:32:37 +0530

    login linked to Api

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, use } from "react";
 // import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
@@ -14,8 +14,9 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
-    const navigate = useNavigate();
     const [products, setProducts] = useState([]);
+    const [token,setToken] = useState('');
+    const navigate = useNavigate();
 
 
     const addToCart = async (itemId,size) =>{
@@ -100,13 +101,20 @@ const ShopContextProvider = (props) => {
             getProductsData();
       },[])
 
+      useEffect(()=>{
+          if (!token && localStorage.getItem('token')) {
+            setToken(localStorage.getItem('token'))
+          }
+      },[])
+
 
     const value = {
         products , currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount,updateQuantity,
-        getCartAmount,navigate, backendUrl
+        getCartAmount,navigate, backendUrl,
+        setToken, token
 
 
     }
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,49 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import { toast } from 'react-toastify';
 
 const Login = () => {
   const [currentState, setCurrentState] = useState('Sign Up');
+  const {token,setToken,navigate,backendUrl} = useContext(ShopContext);
+  
+  const [name,setName]= useState('');
+  const [email,setEmail]= useState('');
+  const [password,setPassword]= useState('');
+
   const onSubmitHandler = async(event)=>{
        event.preventDefault(); 
+       try {
+
+        if(currentState === 'Sign Up'){
+         const responce = await axios.post(backendUrl + '/api/user/register',{name,email,password})
+         if (responce.data.success){
+            setToken(responce.data.token)
+            localStorage.setItem('token', responce.data.token)
+         }else{
+           toast.error(responce.data.message)
+         }
+        }else{
+          const responce = await axios.post(backendUrl + '/api/user/login',{email,password})
+          if (responce.data.success){
+            setToken(responce.data.token)
+            localStorage.setItem('token', responce.data.token)
+          }else{
+            toast.error(responce.data.message)
+          }
+        }
+       } catch (error) {
+         console.log(error)
+         toast.error(message)
+       }
   }
+
+
+     useEffect(()=>{
+       if (token) {
+          navigate('/')
+        }
+     },[token])
 
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
@@ -12,7 +51,9 @@ const Login = () => {
         <p className='prata-regular text-3xl'>{currentState}</p>
         <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
       </div>
-      {currentState === 'Login' ? '' :<input 
+      {currentState === 'Login' ? '' :
+       <input 
+        onChange={(e)=>setName(e.target.value)} value={name}
         type="text" 
         className="w-full px-3 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-colors duration-200" 
         placeholder="Name" 
@@ -20,6 +61,7 @@ const Login = () => {
       /> }  
 
       <input 
+        onChange={(e)=>setEmail(e.target.value)} value={email}
         type="email" 
         className="w-full px-3 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-colors duration-200" 
         placeholder="Email" 
@@ -27,6 +69,7 @@ const Login = () => {
       />
 
       <input 
+        onChange={(e)=>setPassword(e.target.value)} value={password}
         type="password" 
         className="w-full px-3 py-2 border border-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-colors duration-200" 
         placeholder="Password" 
