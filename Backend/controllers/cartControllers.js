3f802c83011d0864b7202a1209d950d1b63cbe5b
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-10-28 01:02:29 +0530

    Add products to user cart API

--- Backend/controllers/cartControllers.js
+++ Backend/controllers/cartControllers.js
@@ -4,6 +4,33 @@
 
 const  addToCart = async (req,res)=>{
 
+  try {
+      const {userId, itemId, size} = req.body
+    const userData = await userModel.findById(userId)
+     let cartData = userData.cartData;
+     if (cartData[itemId]){
+
+        if (cartData[itemId][size]){
+            cartData[itemId][size] += 1
+        }else{
+            cartData[itemId][size] = 1
+        }
+     } else{
+        cartData[itemId] = {}
+        cartData[itemId][size] = 1
+     }
+
+     await userModel.findByIdAndUpdate(userId, {cartData})
+
+     res.json({success: true, message: "Added to Cart"})
+    
+  } catch (error) {
+
+    console.log(error)
+    res.json({success: false,  message: error.message})
+    
+  }
+
 }
 
 
